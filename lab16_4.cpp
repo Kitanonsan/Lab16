e92d60d467feb0c86cd0d746ae6bdef4ac887da9
Non <Kitanon_san@cmu.ac.th>
2022-01-14 17:55:51 +0700

    Lab16_4

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,31 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &m1,int &m2,int &m3,int &m4){
+	int x[] = {m1,m2,m3,m4};
+	int I1 = rand()%4;
+	int I2;
+	while (true){
+		I2 = rand()%4;
+		if(I2 != I1)
+			break;	
+	}
+	int I3;
+	while (true){
+		I3 = rand()%4;
+		if(I3 != I1 && I3 != I2 )
+			break;	
+	}
+	int I4;
+	while (true){
+		I4 = rand()%4;
+		if(I4 != I1 && I4 != I2 && I4 != I3 )
+			break;	
+	}
+
+	m1 = x[I1];
+	m2 = x[I2];
+	m3 = x[I3];
+	m4 = x[I4];
+}
